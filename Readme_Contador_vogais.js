44851204a9ed6c98c0844bb5c9a57abf7cbe8eb0
Vitorverg97 <vitor.vsouza05@gmail.com>
2024-10-28 14:35:32 -0300

    Create Readme_Contador_vogais.js

---
+++ Readme_Contador_vogais.js
@@ -0,0 +1,19 @@
+# Contador de Vogais
+Objetivo: 
+Crie uma função que conte o número de vogais em uma string fornecida.
+
+## Instruções:
+Defina uma função chamada contarVogais que receba uma string como argumento.
+A função deve contar e retornar o número total de vogais (a, e, i, o, u) na string, independentemente de serem maiúsculas ou minúsculas.
+Use um loop para iterar sobre cada caractere da string.
+Utilize um array ou string para armazenar as vogais.
+
+## Exemplo de Uso:
+```.js
+console.log(contarVogais("Hello World")); // Saída: 3
+console.log(contarVogais("JavaScript é divertido")); // Saída: 8
+console.log(contarVogais("")); // Saída: 0
+```
+### Dicas:
+Você pode usar o método toLowerCase() para facilitar a comparação.
+Pense em como você pode percorrer a string e verificar se cada caractere é uma vogal.
